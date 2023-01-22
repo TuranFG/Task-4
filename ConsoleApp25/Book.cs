9d9a94241d9a3d72d03cbd6302f9af9624c0a88e
TuranFG <turanfg@code.edu.az>
2023-01-22 23:18:23 +0400

    Add project files.

---
+++ ConsoleApp25/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Book
+    {
+        internal string Name;
+        internal string AuthorName;
+        internal int PublishedDate;
+        internal int PageCount;
+
+
+
+        public override string ToString()
+        {
+            return $"{Name} {AuthorName} {PublishedDate} {PageCount}";
+        }
+    }
+
+    internal class Book2
+    {
+        internal string Name;
+        internal string AuthorName;
+        internal int PublishedDate;
+        internal int PageCount;
+    }
+}
---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookOverRide();
+
+
+        }
+        static void BookOverRide()
+        {
+            Console.Write("A number of books: ");
+            int n = int.Parse(Console.ReadLine());
+            Book[] books = new Book[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Book book = new Book();
+                Console.Write($"{i + 1}. Book Name: ");
+                book.Name = Console.ReadLine();
+                Console.Write($"{i + 1}. Author Name: ");
+                book.AuthorName = Console.ReadLine();
+                Console.Write($"{i + 1}. Published Date: ");
+                book.PublishedDate = int.Parse(Console.ReadLine());
+            l1: Console.Write($"{i + 1}. Page Count: ");
+                book.PageCount = int.Parse(Console.ReadLine());
+                if (book.PageCount > 10 == false)
+                {
+                    Console.WriteLine("Page numer is more than this ");
+                    goto l1;
+                }
+                Console.WriteLine();
+
+                books[i] = book;
+            }
+
+
+            foreach (var item in books)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        static void Book2()
+        {
+
+            Console.Write("A number of books: ");
+            int n = int.Parse(Console.ReadLine());
+            Book2[] books = new Book2[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Book2 book = new Book2();
+                Console.Write($"{i + 1}. Book Name: ");
+                book.Name = Console.ReadLine();
+                Console.Write($"{i + 1}. Author Name: ");
+                book.AuthorName = Console.ReadLine();
+                Console.Write($"{i + 1}. Published Date: ");
+                book.PublishedDate = int.Parse(Console.ReadLine());
+            l1: Console.Write($"{i + 1}. Page Count: ");
+                book.PageCount = int.Parse(Console.ReadLine());
+                if (book.PageCount > 10 == false)
+                {
+                    Console.WriteLine("Page numer is more than this ");
+                    goto l1;
+                }
+                Console.WriteLine();
+
+                books[i] = book;
+
+                Console.WriteLine($"Book Name: {book.Name}");
+                Console.WriteLine($"Author Name: {book.AuthorName}");
+                Console.WriteLine($"Published Date: {book.PublishedDate}");
+                Console.WriteLine($"Page Count: {book.PageCount}");
+                Console.WriteLine();
+
+            }
+                 
+
+
+        }
+
+    }
+}
